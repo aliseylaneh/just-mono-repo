fb94e1134adcb3018a850583017911bb903c3345
aliseylaneh <aliseylaneh@gmail.com>
2024-05-13 23:44:42 +0330

    testing variable assigning

--- go/algos_datastructure/hello.go
+++ go/algos_datastructure/hello.go
@@ -5,7 +5,10 @@ import "fmt"
 func main() {
 	var greetingText string = "Hello Go"
 	counter := 10
-	for i := 0; i < counter; i++ {
+	for i := 0; i < 5; i++ {
+		counter += 10
 		fmt.Println(greetingText)
 	}
+	finalCounter := counter
+	print(finalCounter)
 }
