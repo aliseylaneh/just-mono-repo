d1455eb1cc7139139b4ab5f1c175b75b80b0fbec
aliseylaneh <aliseylaneh@gmail.com>
2024-05-14 20:50:00 +0330

    testing variable concatination

--- go/algos_datastructure/hello.go
+++ go/algos_datastructure/hello.go
@@ -2,13 +2,21 @@ package main
 
 import "fmt"
 
+func concatinate_types(defaultDataBaseUrl string, defaultDatabaseUserAndPort string) string {
+	concatinated := defaultDataBaseUrl + defaultDatabaseUserAndPort
+	return concatinated
+}
+
 func main() {
-	var greetingText string = "Hello Go"
-	counter := 10
-	for i := 0; i < 5; i++ {
-		counter += 10
-		fmt.Println(greetingText)
-	}
-	finalCounter := counter
-	print(finalCounter)
+	// var greetingText string = "Hello Go"
+	// counter := 10
+	// for i := 0; i < 5; i++ {
+	// 	counter += 10
+	// 	fmt.Println(greetingText)
+	// }
+	// finalCounter := counter
+	// print(finalCounter)
+	defaultDataBaseUrl := "psql:localhost@"
+	var defaultDatabaseUserAndPort string = "admin:5329"
+	fmt.Println(concatinate_types(defaultDataBaseUrl, defaultDatabaseUserAndPort))
 }
