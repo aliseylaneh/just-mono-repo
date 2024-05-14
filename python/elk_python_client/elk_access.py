b92ff20ca4406409eb5b3e1d98f0d0ec58f94950
aliseylaneh <aliseylaneh@gmail.com>
2024-05-14 15:29:01 +0330

    using async elasticsearch client and testing async context manager for elasticsearch client

--- python/elk_python_client/elk_access.py
+++ python/elk_python_client/elk_access.py
@@ -1,19 +1,49 @@
 from datetime import datetime
-from elasticsearch import Elasticsearch
+from elasticsearch import AsyncElasticsearch
+import asyncio
+from typing import Any
 
-if __name__ == "__main__":
-    client = Elasticsearch(
+
+async def insert(
+    client: AsyncElasticsearch, index_name: str, identifier: int, body: dict[str, Any]
+):
+    resp = await client.index(
+        index=index_name, id=identifier, document=body, pretty=True
+    )
+    return resp
+
+
+async def find_all(
+    client: AsyncElasticsearch,
+    index_name: str,
+    pagination_size: int,
+    pagination_index: int,
+) -> list[dict[str, Any]]:
+    resp = await client.search(
+        index=index_name, size=pagination_size, from_=pagination_index, pretty=True
+    )
+    result = []
+    for item in resp.body["hits"]["hits"]:
+        result.append(item["_source"])
+    print(result)
+    return result
+
+
+async def main():
+    client = AsyncElasticsearch(
         "http://localhost:9200",
     )
-
     doc = {
         "author": "author_name",
         "text": "Interesting content...",
         "timestamp": datetime.now(),
     }
-    resp = client.index(index="test-index", id=3, document=doc)
-    print(resp["result"])
+    async with client:
+        await insert(client=client, identifier=1, index_name="books", body=doc)
+        await find_all(
+            client=client, index_name="books", pagination_index=0, pagination_size=2
+        )
 
-    resp = client.search(index="test-index", size=1, from_=0)
-    for hit in resp["hits"]["hits"]:
-        print(hit["_source"])
+
+if __name__ == "__main__":
+    asyncio.run(main())
