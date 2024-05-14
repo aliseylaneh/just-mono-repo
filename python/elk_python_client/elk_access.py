4f3fcb3af8ec47405dc7e2c5769dee5d62f493dd
aliseylaneh <aliseylaneh@gmail.com>
2024-05-14 09:40:46 +0330

    testing pagination using search

--- python/elk_python_client/elk_access.py
+++ python/elk_python_client/elk_access.py
@@ -1,11 +1,13 @@
 from datetime import datetime
+from random import random
+import string
 from elasticsearch import Elasticsearch
-
 
 if __name__ == "__main__":
     client = Elasticsearch(
         "http://localhost:9200",
     )
+ 
     doc = {
         "author": "author_name",
         "text": "Interesting content...",
@@ -14,6 +16,6 @@ if __name__ == "__main__":
     resp = client.index(index="test-index", id=3, document=doc)
     print(resp["result"])
 
-    resp = client.search(index="test-index")
+    resp = client.search(index="test-index", size=1, from_=0)
     for hit in resp["hits"]["hits"]:
         print(hit["_source"])
