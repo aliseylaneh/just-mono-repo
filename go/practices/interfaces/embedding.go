59ae591ed4e71693935cedae9df93b348fa8cb57
aliseylaneh <aliseylaneh@gmail.com>
2024-11-10 15:59:59 +0330

    Struct embedding

--- go/practices/interfaces/embedding.go
+++ go/practices/interfaces/embedding.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type TypeOneInterface interface {
 	write()
 }
@@ -16,4 +18,21 @@ type EmbeddedInterface interface {
 func testEmbeddedInterfaces(value EmbeddedInterface) {
 	value.get()
 	value.write()
+}
+
+type TypeOneStruct struct {
+	name string
+}
+type TypeTwoStruct struct {
+	userInput string
+	*TypeOneStruct
+}
+
+func (s *TypeOneStruct) write() {
+	fmt.Println(s.name)
+}
+func testEmbeddedStruct() {
+	typeTwoStruct := TypeTwoStruct{userInput: "'git checkout -b feature/newBranch'",
+		TypeOneStruct: &TypeOneStruct{name: "Ali Seylaneh"}}
+	typeTwoStruct.write()
 }
