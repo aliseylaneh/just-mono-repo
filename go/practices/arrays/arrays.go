628b0d165d78b4cb9323847d45aad0c870aac47b
aliseylaneh <aliseylaneh@gmail.com>
2024-11-20 13:55:49 +0330

    comparing slices using standard library slices.Equal function

--- go/practices/arrays/arrays.go
+++ go/practices/arrays/arrays.go
@@ -1,9 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"slices"
+)
+
 func main() {
+	// Arrays
 	var x = [...]int{23, 123, 12, 44, 5}
 	x[4] = 54
+	// Multi dimensional arrays
 	var y = [2][3]int{}
 	y[0] = [3]int{23, 22, 41}
 	y[1] = [3]int{23, 22, 41}
+	// Slices
+	sliceX := []int{23, 11, 3321, 11}
+	sliceY := []int{223, 122, 331, 22}
+	fmt.Println(slices.Equal(sliceX, sliceY))
+	sliceXY := []int{1, 2, 3, 4}
+	sliceYY := []int{1, 2, 3, 4}
+	fmt.Println(slices.Equal(sliceXY, sliceYY))
+
 }
