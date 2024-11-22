9852612e25f0a7019a593c0abcf04db6fcc2e8ce
aliseylaneh <aliseylaneh@gmail.com>
2024-11-22 14:20:02 +0330

    shadowing variable description

--- go/practices/blocks/functions.go
+++ go/practices/blocks/functions.go
@@ -7,6 +7,7 @@ func main() {
 	if x > 5 {
 		fmt.Println(x)
 		x := 5 // This will shadow the upper block variable x, but it's not going to be accessible in another blocks
+		// Also if you shadow a variable you can't access the original variable at all
 		fmt.Println(x)
 	}
 	fmt.Println(x)
