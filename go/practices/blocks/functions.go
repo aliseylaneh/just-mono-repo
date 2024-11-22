8e0e0d5f2e5b73154ccf18452b418cdfdec0c789
aliseylaneh <aliseylaneh@gmail.com>
2024-11-22 19:22:00 +0330

    Breaking a loop from a switch within using label

--- go/practices/blocks/functions.go
+++ go/practices/blocks/functions.go
@@ -55,4 +55,18 @@ outer:
 		}
 		fmt.Println()
 	}
+loop:
+	for i := 0; i < 10; i++ {
+		switch i {
+		case 0, 2, 4, 6:
+			fmt.Println(i, "is even")
+		case 3:
+			fmt.Println(i, "is divisible by 3 but not 2")
+		case 7:
+			fmt.Println("exit the loop!")
+			break loop
+		default:
+			fmt.Println(i, "is boring")
+		}
+	}
 }
