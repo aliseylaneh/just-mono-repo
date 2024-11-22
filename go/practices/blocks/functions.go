fb09960538df768a47f8cfd9fcdf90243b5db7e8
aliseylaneh <aliseylaneh@gmail.com>
2024-11-22 18:58:39 +0330

    Break outer loop

--- go/practices/blocks/functions.go
+++ go/practices/blocks/functions.go
@@ -39,7 +39,20 @@ func main() {
 		"Jake": "Product manager",
 		"John": "QA",
 	}
+
 	for name, position := range customers {
 		fmt.Println(name, position)
+
+	}
+	samples := []string{"hello", "apple_π!"}
+outer:
+	for _, sample := range samples {
+		for i, r := range sample {
+			fmt.Println(i, r, string(r))
+			if r == 'l' {
+				continue outer
+			}
+		}
+		fmt.Println()
 	}
 }
