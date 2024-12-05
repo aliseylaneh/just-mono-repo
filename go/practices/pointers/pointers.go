38947521239d21ff5c74c039876e927f0372d69d
aliseylaneh <aliseylaneh@gmail.com>
2024-12-05 15:01:36 +0330

    failed update on value of a passed paramter pointer

--- go/practices/pointers/pointers.go
+++ go/practices/pointers/pointers.go
@@ -15,10 +15,18 @@ func typeChecker(arg interface{}) {
 		fmt.Println("Type is a pointer product")
 	}
 }
+func failedUpdate(g *int) {
+	x := 10
+	g = &x
+}
+
 func makePointer[T any](t T) *T {
 	return &t
 }
 func main() {
-	product := &Product{name: makePointer("name"), address: "test"}
-	fmt.Println(*product)
+	//product := &Product{name: makePointer("name"), address: "test"}
+	//fmt.Println(*product)
+	var g *int
+	failedUpdate(g)
+	fmt.Println(g)
 }
