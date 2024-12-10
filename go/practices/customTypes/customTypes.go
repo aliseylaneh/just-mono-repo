5ba1abec4cfd316f90f462ccfd73cfa2291c30ca
aliseylaneh <aliseylaneh@gmail.com>
2024-12-10 17:21:35 +0330

    calling point reciver methods changes the original if we pass point type into another function

--- go/practices/customTypes/customTypes.go
+++ go/practices/customTypes/customTypes.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
 type Person struct {
 	name  string
@@ -26,8 +23,8 @@ type Inventory struct {
 	stock uint
 }
 
-func (i Inventory) create(id string) {
-	//  No overloading is permitted for a same type methods
+func (i *Inventory) create(id string) {
+	i.id = id
 }
 
 func (i Person) create(id string, name string) {
@@ -38,6 +35,15 @@ type inventories map[string]Inventory
 
 type getInventories func(id string) (Inventory, error)
 
+func doUpdateWrong(inventory Inventory) {
+	inventory.create("hello")
+	fmt.Println(inventory)
+}
+
+func doUpdateRight(inventory *Inventory) {
+	inventory.create("Maoo")
+	fmt.Println(inventory)
+}
 func main() {
 	//var people personData = personData{
 	//	"Person Number one": Person{name: "Ali", phone: 2311},
@@ -46,21 +52,28 @@ func main() {
 	//var myNumber customInt = 2
 	//result := myNumber.pow(2)
 	//fmt.Println(result)
-	var myFunc getInventories = func(id string) (Inventory, error) {
-		listOfInventory := []Inventory{{id: "SKI-TEST", stock: 32}, {id: "SKU-2", stock: 44}}
-		for _, inventory := range listOfInventory {
-			if inventory.id == id {
-				return inventory, nil
-			}
-		}
-		return Inventory{}, errors.New("Inventory not found !!")
-	}
+	//var myFunc getInventories = func(id string) (Inventory, error) {
+	//	listOfInventory := []Inventory{{id: "SKI-TEST", stock: 32}, {id: "SKU-2", stock: 44}}
+	//	for _, inventory := range listOfInventory {
+	//		if inventory.id == id {
+	//			return inventory, nil
+	//		}
+	//	}
+	//	return Inventory{}, errors.New("Inventory not found !!")
+	//}
+	//
+	//inventory, err := myFunc("SKO-TEST")
+	//if err == nil {
+	//	fmt.Println(inventory)
+	//} else {
+	//	fmt.Println(err)
+	//}
 
-	inventory, err := myFunc("SKO-TEST")
-	if err == nil {
-		fmt.Println(inventory)
-	} else {
-		fmt.Println(err)
-	}
+	var inventory Inventory
+	doUpdateWrong(inventory)
+	fmt.Println(inventory)
+	fmt.Println("-------")
+	doUpdateRight(&inventory)
+	fmt.Println(inventory)
 
 }
