3f4465e255e72bd976f063a6a80af96a88430941
aliseylaneh <aliseylaneh@gmail.com>
2024-12-11 11:52:03 +0330

    add new comment of using iota for constant which are cared value for databases

--- go/practices/iota/constants.go
+++ go/practices/iota/constants.go
@@ -12,6 +12,9 @@ const (
 // and the defining const with this type and iota
 type Role int
 
+// using iota with constants is fragile when you care about the value. You
+// don’t want a future maintainer to insert a new constant in the
+// middle of the list and break your code. From Go learning.
 const (
 	Admin Role = iota + 1
 	User
