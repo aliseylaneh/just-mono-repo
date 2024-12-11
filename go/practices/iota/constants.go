330b9c323e9aa375c1ec4fc33433eca4c0b5bb10
aliseylaneh <aliseylaneh@gmail.com>
2024-12-11 11:46:18 +0330

    using iota for simulating emumeration

--- go/practices/iota/constants.go
+++ go/practices/iota/constants.go
@@ -8,6 +8,16 @@ const (
 	JustAnotherValue
 )
 
+// Best practice for simulating an enumeration in go is defining a custom type and the defining const with this type and iota
+type Role int
+
+const (
+	Admin Role = iota
+	User
+	Customer
+	Reviewer
+)
+
 func main() {
 	fmt.Println(AttackHeal, AttackDamage, JustAnotherValue)
 }
