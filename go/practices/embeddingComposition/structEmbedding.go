89569e90ff78a94dd2ced3d0e1e9b2fdc42efb21
aliseylaneh <aliseylaneh@gmail.com>
2024-12-11 14:16:23 +0330

    inner method calling

--- go/practices/embeddingComposition/structEmbedding.go
+++ go/practices/embeddingComposition/structEmbedding.go
@@ -3,19 +3,29 @@ package main
 import "fmt"
 
 type Inner struct {
-	name string
+	A int
+}
+
+func (i Inner) IntPrinter(val int) string {
+	return fmt.Sprintf("Inner: %d", val)
+}
+func (i Inner) Double() string {
+	return i.IntPrinter(i.A * 2)
 }
 
 type Outer struct {
 	Inner
-	name string
+	S string
+}
+
+func (o Outer) IntPrinter(val int) string {
+	return fmt.Sprintf("Outer: %d", val)
 }
 
 func main() {
 	o := Outer{
-		Inner: Inner{"Ali"},
-		name:  "Jack",
+		Inner: Inner{A: 10},
+		S:     "Hello",
 	}
-	fmt.Println(o.name)
-	fmt.Println(o.Inner.name)
+	fmt.Println(o.Double())
 }
